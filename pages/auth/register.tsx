a5e7fc0215219ee44c295be99abf281967ad3c73
Tobey14 <sureboytobi@gmail.com>
2022-11-08 15:10:35 +0100

    Registration Fixes

--- pages/auth/register.tsx
+++ pages/auth/register.tsx
@@ -1,18 +1,48 @@
-import React from 'react'
+import React, {useState} from 'react'
+import axios from 'axios'
+
 
 const register = () => {
+
+  const [inputs, setInputs] = useState({
+    fullName: '',
+    email: '',
+    password: '',
+    
+  });
+
+  const handleChange = (event:  React.ChangeEvent<HTMLInputElement>) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs(values => ({...values, [name]: value}))
+  }
+
+  const handleSubmit = async (event:  React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+   
+      try {
+        const response = await axios.post('/api/auth', inputs);
+        setInputs({
+          fullName: '',
+          email: '',
+          password: '',
+        })
+
+
+      } catch (error) {
+        alert(error);
+      }
+  }
+
   return (
     <div className='login'>
         <p className="welcome">Welcome To PM System, Enter Detail To Create Organization Account</p>
 
         <div className="form">
-            <input type="text" placeholder='Enter Full Name' />
-            <input type="text" placeholder='Enter Email' />
-            <input type="text" placeholder='Enter Password' />
-            <input type="text" placeholder='Enter Organization Name' />
-            <input type="text" placeholder='Describe your organization' /> <br />
-
-            <button className="btn-primary">Register</button>
+            <input type="text" onChange={handleChange} name="fullName" value={inputs.fullName || ""} placeholder='Enter Full Name' />
+            <input type="text" onChange={handleChange} placeholder='Enter Email' name="email" value={inputs.email || ""} />
+            <input type="text" onChange={handleChange} placeholder='Enter Password'  name="password" value={inputs.password || ""}/> <br />
+            <button className="btn-primary" onClick={handleSubmit}>Register</button>
 
         </div>
     </div>
